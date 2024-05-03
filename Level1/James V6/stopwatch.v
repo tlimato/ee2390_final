568fa7314c0ee52771da9eb9d45220beedea5c3f
Tyson Limato <72205469+tlimato@users.noreply.github.com>
2024-05-03 09:07:30 -0600

    Update stopwatch.v

--- Level1/James V6/stopwatch.v
+++ Level1/James V6/stopwatch.v
@@ -120,21 +120,56 @@ always @ (*)
 assign an = an_temp;
 
 reg [6:0] sseg_temp; 
+// IMPLIMENT FLASH ENABLE
+reg flash_enable 1'b0;
 always @ (*)
  begin
   case(sseg)
-   4'd0 : sseg_temp = 7'b1000000;
-   4'd1 : sseg_temp = 7'b1111001;
-   4'd2 : sseg_temp = 7'b0100100;
-   4'd3 : sseg_temp = 7'b0110000;
-   4'd4 : sseg_temp = 7'b0011001;
-   4'd5 : sseg_temp = 7'b0010010;
-   4'd6 : sseg_temp = 7'b0000010;
-   4'd7 : sseg_temp = 7'b1111000;
-   4'd8 : sseg_temp = 7'b0000000;
-   4'd9 : sseg_temp = 7'b0010000;
-   default : sseg_temp = 7'b0111111; //dash
+   4'd0 : begin 
+       sseg_temp = 7'b1000000;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd1 : begin 
+       sseg_temp = 7'b1111001;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd2 : begin
+       sseg_temp = 7'b0100100;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd3 : begin
+       sseg_temp = 7'b0110000;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd4 : begin
+       sseg_temp = 7'b0011001;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd5 : begin 
+       sseg_temp = 7'b0010010;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd6 : begin 
+       sseg_temp = 7'b0000010;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd7 : begin
+       sseg_temp = 7'b1111000;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd8 : begin 
+       sseg_temp = 7'b0000000;
+       flash_enable = 1'b0; // Enable flashing //dash
+   4'd9 : begin 
+       sseg_temp = 7'b0010000;
+       flash_enable = 1'b0; // Enable flashing //dash
+      default: begin
+          sseg_temp = 7'b0111111;
+          flash_enable = 1'b1; // Enable flashing //dash
+      end
   endcase
  end
+// Tyson Flash Seven Segment.w
+always @(flash_enable) begin
+    if (flash_enable) // If flashing is enabled
+        sseg_temp_seg = 7'b1000000; // Display 0
+    else
+        sseg_temp_seg = 7'b1111111; // Turn off all segments
+end 
+       
 assign {g, f, e, d, c, b, a} = sseg_temp; 
 assign dp = reg_dp;
+
+endmodule
